01d7f9c0c3ebe3e7f9f3b7a22ca664acff382bfa
AlexZorzi <info@alexzorzi.it>
2021-03-27 02:13:31 +0100

    dependencies and import fix

--- lib/widgets/HomeCard.dart
+++ lib/widgets/HomeCard.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import '../pages/animeInfo.dart';
